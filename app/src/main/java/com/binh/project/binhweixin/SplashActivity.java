cb8d088fb585ac8d26ad238c3b4d38fd18049912
binh <binh1990@126.com>
2016-11-17 23:05:27 +0800

    初始化

---
+++ app/src/main/java/com/binh/project/binhweixin/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.binh.project.binhweixin;
+
+import android.os.Bundle;
+
+/**
+ * Created by binh on 2016/11/17.
+ */
+
+public class SplashActivity extends BinhActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
