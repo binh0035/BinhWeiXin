3847e8e3ed97a4fe1caea2020aa430c63dba00ab
binh <binh1990@126.com>
2016-11-17 23:04:24 +0800

    初始化

--- app/src/main/java/com/binh/project/binhweixin/MainActivity.java
+++ app/src/main/java/com/binh/project/binhweixin/MainActivity.java
@@ -1,9 +1,8 @@
 package com.binh.project.binhweixin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BinhActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
